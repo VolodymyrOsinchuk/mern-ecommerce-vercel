35f6021f6861cbd4c6306b4c9075809f88ca4528
Volodymyr <volodymyr@MacBook-Air-de-Volodymyr.local>
2025-03-11 11:32:11 +0100

    update to React 19

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
-import MainRouter from "./MainRouter";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core/styles";
-import theme from "./theme";
+// import React from "react";
+// import MainRouter from "./MainRouter";
+// import { BrowserRouter } from "react-router-dom";
+// import { ThemeProvider } from "@material-ui/core/styles";
+// import theme from "./theme";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <ThemeProvider theme={theme}>
+//         <MainRouter />
+//       </ThemeProvider>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+import React from 'react'
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <MainRouter />
-      </ThemeProvider>
-    </BrowserRouter>
-  );
-};
+  return <div>App</div>
+}
 
-export default App;
+export default App
